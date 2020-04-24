21bcf8e1af6129da682167bea58d8676fa3946a0
ranraok <46853842+ranraok@users.noreply.github.com>
2020-04-23 21:27:00 -0400

    completed MVP

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const server = require('./server')
 const db = require('../data/dbConfig');
+const Foods = require('../food/foodModel')
 
 describe('server router functions', () => {
     describe('GET @ /foods ', () => {
@@ -18,19 +19,42 @@ describe('server router functions', () => {
         })
 
     describe('POST @ /', () => {
-       beforeEach(async () => {
-           await db('food').truncate();
-       })
-       it("return 201 on success", () => {
-           return request(server)
-            .post('/')
-            .send({name: 'chicken'})
-            .then(res => {
-                expect(res.status).toBe(201)
-            })
-       })
+        beforeEach(async () => {
+            await db('foods').truncate();
+        });
+        it('should add the food', async () => {
+            await Foods.addFood({name: 'candy', protein: '0', carbs: '0', fat: '0'})
 
+            const foods = await db('foods');
+
+            expect(foods).toHaveLength(1);
+        })
+        it('should add the provided food to the database', async () => {
+            let food = await Foods.addFood({name: 'candy', protein: '0', carbs: '0', fat: '0'})
+            expect(food.name).toBe('candy')
+        })
     })
-    describe('DELETE @ /foods/:id', () => {
+
+    describe('DELETE', () => {
+        beforeEach (async () => {
+            await db('foods').truncate();
+        })
+        it('should delete a food from the list', async () => {
+            await Foods.addFood({name: 'candy', protein: '0', carbs: '0', fat: '0'})
+            await Foods.addFood({name: 'corn', protein: '0', carbs: '0', fat: '0'})
+            await Foods.addFood({name: 'soda', protein: '0', carbs: '0', fat: '0'})
+    
+            await Foods.deleteFood(1);
+
+            const foods = await db('foods');
+    
+            expect(foods.length).toBe(2);
+        })
+        it('should return JSON', () => {
+            return request(server).get('/')
+            .then(res => {
+                expect(res.type).toMatch(/json/i);
+            })
+        })
     })
 })
